8376c546b1d8a1cae0e8783ad69ec02d2ebcdaa9
tazawa rei <taza_rei@icloud.com>
2024-08-29 11:51:58 +0900

    add Enemyの動きと当たり判定を追加

--- Assets/Scripts/EnemyScripts.cs
+++ Assets/Scripts/EnemyScripts.cs
@@ -5,24 +5,38 @@ using UnityEngine;
 
 public class EnemyScripts : MonoBehaviour
 {
+    GameObject player_;
     // Start is called before the first frame update
     void Start()
     {
-        Invoke(nameof(EnemyScripts), 3.5f);
+        this.player_ = GameObject.Find("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(0.1f, 0, 0);//1フレームごとに等速で直進
+        transform.Translate(0.08f, 0, 0);//1フレームごとに等速で直進
 
         //指定の位置に来たら壊す
-        if(transform.position.x >60f)
+        if (transform.position.x > 60f)
         {
             Destroy(gameObject);
         }
 
+
+
+        //当たり判定
+        Vector2 p1 = transform.position;
+        Vector2 p2 = this.player_.transform.position;
+        Vector2 dir = p1 - p2;
+        float d = dir.magnitude;
+        float r1 = 0.5f;
+        float r2 = 1.0f;
+
+        if(d < r1 + r2)
+        {
+            Destroy (gameObject);
+        }
+
     }
-
-
 }
